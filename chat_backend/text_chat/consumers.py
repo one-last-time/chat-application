895c09407819c4c154b127cce20e36607f17c5df
Shaffat-BS23 <Shaffat.Nur@brainstation-23.com>
2023-05-16 16:37:00 +0600

    brodcast message in group

--- chat_backend/text_chat/consumers.py
+++ chat_backend/text_chat/consumers.py
@@ -1,15 +1,34 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class chatConsumer(WebsocketConsumer):
     def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = "chat_%s" % self.room_name
+        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
         self.accept()
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat.message',
+                'message': ':'.join([str(x) for x in self.scope.get('client')])+' has joined the chat',
+            }
+        )
 
     def disconnect(self, code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        self.send(json.dumps({'message': message}))
-    
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {
+                "type": "chat.message",
+                "message": message
+            }
+        )
+
+    def chat_message(self, event):
+        message = event["message"]
+        self.send(text_data= json.dumps({"message": message}))
