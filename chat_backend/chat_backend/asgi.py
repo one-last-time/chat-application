67d3cd3e7a619d4d51876d6d3bb4adacaa58d67e
one-last-time <msdipu12@gmail.com>
2023-05-16 08:03:04 +0600

    consumer added

--- chat_backend/chat_backend/asgi.py
+++ chat_backend/chat_backend/asgi.py
@@ -8,13 +8,20 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_backend.settings')
 
+from .routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter(
     {
-        'http': get_asgi_application()
+        'http': get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        )
     }
 )
---
+++ chat_backend/chat_backend/routing.py
@@ -0,0 +1,3 @@
+from text_chat.routing import websocket_urlpatterns as textchat_websokets
+websocket_urlpatterns = []
+websocket_urlpatterns += textchat_websokets
